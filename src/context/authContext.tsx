08ac781d8def3d2a84195f1cfcc1d9b8d47e5ab0
Beka Haile <barbeka.tola@gmail.com>
2023-05-20 08:37:40 +0400

    Auth context added

---
+++ src/context/authContext.tsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { createContext, useEffect, useState, ReactNode } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+interface AuthContextType {
+  currentUser: User | null;
+  login: (inputs: any) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      const user = localStorage.getItem("user");
+      if (user) setCurrentUser(JSON.parse(user));
+    }
+  }, []);
+
+  const login = async (inputs: any) => {
+    const res = await axios.post("/auth/login", inputs);
+    setCurrentUser(res.data);
+  };
+
+  const logout = async () => {
+    await axios.post("/auth/logout");
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
